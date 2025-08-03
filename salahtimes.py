c767d2f7ac72b18689ead36caa790e5279941572
bruciexx <brucegooijer@gmail.com>
2025-08-03 04:47:28 +0200

    testing

--- salahtimes.py
+++ salahtimes.py
@@ -1,5 +1,5 @@
-from suncalc import get_position, get_times     # module for calculating sun position and sunlight phases
-from datetime import datetime, timezone         # module for getting the current date and time from the system
+from suncalc import get_position, get_times     # module that calcs pos of sun
+from datetime import datetime, timezone
 import pandas as pd                             # module for data manipulation
 
 ''' setting initial initial variables '''
@@ -11,6 +11,8 @@ lat = 52.375
 times = get_times(date, lon, lat)
 position = get_position(date, lon, lat)
 
+solar_phase = 'solar_noon'
+
 ''' testing grounds '''
 
 
@@ -19,6 +21,13 @@ df = pd.DataFrame({
     'lon': [lat],
     'lat': [lat]
 })
-print(date)
-print(pd.DataFrame(get_position(df['date'], df['lon'], df['lat'])))
 
+print("current date and time (UTC) is:")
+print(date)
+print(20 * '-')
+print("current sun position is:")
+print(pd.DataFrame(get_position(df['date'], df['lon'], df['lat'])))
+print(20 * '-')
+print("the time for " + solar_phase + " is at:")
+print(pd.DataFrame(get_times(df['date'], df['lon'], df['lat']))[solar_phase])
+print(20 * '-')
